3550941030f8e6e8421a36a5b2fcd5f84c46ec6a
Eric Schulte <eric.schulte@gmx.com>
2012-05-27 17:18:33 -0600

    better introductory commentary

--- go.el
+++ go.el
@@ -27,11 +27,13 @@
 
 ;;; Commentary:
 
-;; An API for transferring GO moves and data between a number of GO
-;; back ends including the following.
+;; A board-based interface to GO games which may be connected to a
+;; number of GO back-ends through a generic API.  To play a game of GO
+;; against the gnugo back-end run `play-go'.  Current back-ends
+;; include the following.
 ;; - the SGF format
 ;; - the Go Text Protocol (GTP)
-;; - the IGS protocol
+;; - TODO: the IGS protocol
 
 ;;; Code:
 (require 'go-util)
