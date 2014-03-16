7a41f7c61d38436877f464c38508026485efe3a2
Eric Schulte <schulte.eric@gmail.com>
2014-03-15 18:50:05 -0600

    fix copyright headers

--- list-buffer.el
+++ list-buffer.el
@@ -1,29 +1,24 @@
 ;;; list-buffer.el --- view a list as a table in a buffer
 
-;; Copyright (C) 2013 Eric Schulte <schulte.eric@gmail.com>
+;; Copyright (C) 2013  Free Software Foundation, Inc.
 
-;; Author: Eric Schulte <eric.schulte@gmx.com>
+;; Author: Eric Schulte <schulte.eric@gmail.com>
 ;; Created: 2013-08-02
 ;; Version: 0.1
 ;; Keywords: list buffer cl
 
-;; This file is not (yet) part of GNU Emacs.
-;; However, it is distributed under the same license.
-
-;; GNU Emacs is free software; you can redistribute it and/or modify
+;; This software is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation; either version 3, or (at your option)
-;; any later version.
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
 
-;; GNU Emacs is distributed in the hope that it will be useful,
+;; This software is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
 
 ;; You should have received a copy of the GNU General Public License
-;; along with GNU Emacs; see the file COPYING.  If not, write to the
-;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-;; Boston, MA 02110-1301, USA.
+;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Code:
 (eval-when-compile (require 'cl))
